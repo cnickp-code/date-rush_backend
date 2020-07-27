d20f79c2e52556ce81b2f0a73df0866712891bd5
Carlo Paredes <c.nick.paredes@gmail.com>
2020-07-26 18:58:56 -0700

    Finished users router and service functions

---
+++ src/users/users-router.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const UsersService = require('./users-service');
+const usersRouter = express.Router();
+
+const bodyParser = express.json();
+
+usersRouter
+    .post('/', bodyParser, (req, res, next) => {
+        const { password, user_name, email } = req.body;
+        const fields = ['user_name', 'password', 'email'];
+
+        for(const field of fields) {
+            if(!req.body[field]) {
+                return res.status(400).json({
+                    error: `Missing '${field}' in request body`
+                });
+            }
+        }
+
+        const passwordError = UsersService.verifyPassword(password);
+
+        if(passwordError) {
+            return res.status(400).json({ error: passwordError });
+        }
+
+        const emailError = UsersService.verifyEmail(email);
+
+        if(emailError) {
+            return res.status(400).json({ error: emailError });
+        }
+
+        UsersService.hasUserWithUserName(
+            req.app.get('db'),
+            user_name
+        )
+        .then(userBool => {
+            if(!userBool) {
+                return res.status(400).json({
+                    error: `Username already taken`
+                });
+            }
+
+            return UsersService.hashPassword(password)
+                .then(hashedPw => {
+                    const newUser = {
+                        user_name,
+                        password: hashedPw,
+                        email,
+                        date_created: 'now()'
+                    }
+
+                    return UsersService.insertUser(
+                        req.app.get('db'),
+                        newUser
+                    )
+                    .then(user => {
+                        res
+                            .status(201)
+                            .location(`/api/users/${user.id}`)
+                            .json(UsersService.serializeUser(user))
+                    })
+                })
+        })
+        .catch(next);
+    })
+
+module.exports = usersRouter;
