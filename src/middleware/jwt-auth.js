5967342a4c3462d480f27b3899ee9c5b34f47492
Carlo Paredes <c.nick.paredes@gmail.com>
2020-07-26 18:25:28 -0700

    Finished auth endpoints and jwt middleware

---
+++ src/middleware/jwt-auth.js
@@ -0,0 +1,40 @@
+const AuthService = require('../auth/auth-service');
+
+function requireAuth(req, res, next) {
+    const authToken = req.get('Authorization') || '';
+
+    let bearerToken;
+
+    if(!authToken.toLowerCase().startsWith('bearer ')) {
+        return res.status(401).json({ error: 'Missing Bearer Token.' });
+    } else {
+        bearerToken = authToken.slice(7, authToken.length);
+    }
+
+    try {
+        const payload = AuthService.verifyJwt(bearerToken);
+
+        AuthService.getUserWithUserName(
+            req.app.get('db'),
+            payload.sub
+        )
+        .then(user => {
+            if(!user) {
+                return res.status(401).json({ error: 'Unauthorized Request' })
+            }
+            req.user = user;
+            next();
+        })
+        .catch(error => {
+            console.error(error);
+            next(error);
+        })
+
+    } catch(e) {
+        res.status(401).json({ error: 'Unauthorized Request' })
+    }
+}
+
+module.exports = {
+    requireAuth
+};
